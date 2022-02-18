211f29fcf61010680e40297e0a5e3e920c77585e
Tan Nguyen <tannguyen.cse19@gmail.com>
2022-02-18 20:42:39 +0700

    create header, footer + re-organize folder

--- views/common/header.php
+++ views/common/header.php
@@ -1,5 +1,15 @@
 <?php
 include('config/constants.php');
+
+function custom_echo($x, $length)
+{
+  if (strlen($x) <= $length) {
+    echo $x;
+  } else {
+    $y = substr($x, 0, $length) . '...';
+    echo $y;
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +23,7 @@ include('config/constants.php');
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="public/css/global.css">
 
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
@@ -23,7 +34,7 @@ include('config/constants.php');
     <div class="container d-flex justify-content-between align-items-center">
       <div class="">
         <a class="navbar-brand" href="<?php echo SITEURL; ?>">
-          <img src="images/logo.jpg" alt="Restaurant Logo" class="img-responsive img__logo">
+          <img src="public/images/logo.jpg" alt="Restaurant Logo" class="img-responsive img__logo">
         </a>
       </div>
 
